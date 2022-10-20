172dbb1131eab7d72effd4e7c6150a44dcdf6025
virginia <vgiardino@outlook.es>
2022-10-20 17:13:54 -0300

    CRUD checked

--- backend/src/main/java/com/example/piBack/Controller/CategoryController.java
+++ backend/src/main/java/com/example/piBack/Controller/CategoryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-
+@RequestMapping("category")
 public class CategoryController {
 
     @Autowired
