0dc2e47bb8d61c5b57e2709860cbc42bb746fc9c
nkwaaaa <nkwaaaa@gmail.com>
2022-10-20 11:22:00 -0300

    add header update button component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,3 +1,19 @@
+import logo from '../assets/logo.svg';
+import styles from '../styles/header.module.css';
+import Button from './Button';
+
 export default function Header() {
-	return <div>Header</div>;
+	return (
+		<header className={styles.header}>
+			<div>
+				<a href='#'>
+					<img className={styles.header__logo} src={logo} alt='' />
+				</a>
+			</div>
+			<div className={styles.header__btn}>
+				<Button type={'outline'} innerText={'Crear cuenta'} />
+				<Button innerText={'Iniciar sesión'} />
+			</div>
+		</header>
+	);
 }
