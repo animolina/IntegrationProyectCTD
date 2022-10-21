2c89d141221c7c24d794bbf20fd077ea828286ad
animolina <anapaulamolinalopez@gmail.com>
2022-10-21 17:46:22 -0300

    development of SearchBlock component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Body from '../components/Body';
 import Header from './../components/Header';
 import Footer from './../components/Footer';
+import SearchBlock from '../components/SearchBlock';
+import BasicCalendar from '../components/BasicCalendar';
 
 export default function Home() {
 	return (
 		<div>
 			<Header />
+			<SearchBlock />
+			<BasicCalendar />
 			<Body />
 			<Footer />
 		</div>
