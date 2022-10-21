fbfca82d3cdd0bfcef559d2bdd906623d2412ba3
animolina <anapaulamolinalopez@gmail.com>
2022-10-21 18:25:06 -0300

    fixes after development pull

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,12 @@ import Body from '../components/Body';
 import Header from './../components/Header';
 import Footer from './../components/Footer';
 import SearchBlock from '../components/SearchBlock';
-import BasicCalendar from '../components/BasicCalendar';
 
 export default function Home() {
 	return (
 		<div>
 			<Header />
 			<SearchBlock />
-			<BasicCalendar />
 			<Body />
 			<Footer />
 		</div>
