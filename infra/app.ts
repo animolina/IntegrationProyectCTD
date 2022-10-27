3a51b5401fbd1c35e586b31f2a185ec69a980e21
Mauricio Páez <mauricio.paez@sinapsis.co>
2022-10-27 11:00:08 -0500

    Update app.ts

--- infra/app.ts
+++ infra/app.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-import "source-map-support/register";
 import { App } from "aws-cdk-lib";
 import { InfraServices } from "./services";
 
-const app = new App();
-new InfraServices(app);
+try {
+  const app = new App();
+  new InfraServices(app);
+  app.synth();
+} catch (err) {
+  console.log(`Synth Error: ${(err as any).message}`);
+  process.exit(1);
+}
