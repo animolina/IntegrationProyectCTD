b13cd2dc94c1ddf23ef75a8534f42e37a973e380
nkwaaaa <nkwaaaa@gmail.com>
2022-11-04 11:33:02 -0300

    update principal img

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,6 +1,5 @@
 import ProductDetailsHeader from '../components/ProductDetailsHeader';
 import styles from '../styles/productDetails.module.css';
-// eslint-disable-next-line no-unused-vars
 import GalleryProduct from '../utils/GalleryProduct';
 import ProductDetailsLocation from './../components/ProductDetailsLocation';
 import ProductDetailsDescription from './../components/ProductDetailsDescription';
