3710ca807c1c49125f3e8994fc7c4a2bee4118fc
nkwaaaa <nkwaaaa@gmail.com>
2022-11-04 14:45:04 -0300

    add dinamic items

--- frontend/src/components/ProductDetailsFeature.jsx
+++ frontend/src/components/ProductDetailsFeature.jsx
@@ -1,38 +1,19 @@
 import styles from '../styles/productDetailsFeature.module.css';
-import iconTv from '../assets/icons/features/tv.svg';
+
+import ProductDetailsFeatureItem from './ProductDetailsFeatureItem';
 export default function ProductDetailsFeature() {
 	return (
 		<>
 			<h2 className={styles.featureTitle}>¿Qué ofrece este lugar?</h2>
 			<div className={styles.productDetailsFeature}>
 				<ul className={styles.featureGroup}>
-					<li>
-						<img src={iconTv} alt='' /> Cocina
-					</li>
-					<li>
-						<img src={iconTv} alt='' />
-						Televisor
-					</li>
-					<li>
-						<img src={iconTv} alt='' />
-						Aire acondicionado
-					</li>
-					<li>
-						<img src={iconTv} alt='' />
-						Apto mascotas
-					</li>
-					<li>
-						<img src={iconTv} alt='' />
-						Estacionamiento gratuito
-					</li>
-					<li>
-						<img src={iconTv} alt='' />
-						Pileta
-					</li>
-					<li>
-						<img src={iconTv} alt='' />
-						Wifi
-					</li>
+					<ProductDetailsFeatureItem type={'cocina'} />
+					<ProductDetailsFeatureItem type={'televisor'} />
+					<ProductDetailsFeatureItem type={'aire acondicionado'} />
+					<ProductDetailsFeatureItem type={'apto mascotas'} />
+					<ProductDetailsFeatureItem type={'estacionamiento gratuito'} />
+					<ProductDetailsFeatureItem type={'pileta'} />
+					<ProductDetailsFeatureItem type={'wifi'} />
 				</ul>
 			</div>
 		</>
