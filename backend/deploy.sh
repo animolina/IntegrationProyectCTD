8727c818f78e1a35fd1c6cb23482607cb799811d
Mauricio Páez <mauriciol.paez@outlook.com>
2022-11-06 11:24:58 +0000

    Include complete paths on script

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,14 +2,14 @@
 function deploy_api {
     cd ~/grupo-01/backend
     echo "<=============== Fetching last changes... ===============>"
-    git checkout development && git pull
+    git checkout feature/infra-server-setup && git pull
     echo "<=============== Removing old jar file... ===============>"
     rm -rf pi-api.jar
-    cd ..
+    cd ~/grupo-01/backend
     echo "<=============== Compiling project... ===================>"
     mvn clean install
     echo "<=============== Running API... =========================>"
-    cd /target
+    cd ~/grupo-01/backend/target
     nohup java -jar pi-api.jar > process.log 2>&1 &
     echo $! > process-id.txt
 }
