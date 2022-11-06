13293ad88ec2a608fdcf5ff586983490933e3246
nkwaaaa <nkwaaaa@gmail.com>
2022-11-06 17:26:43 -0300

    update style

--- frontend/src/utils/ShareProduct.jsx
+++ frontend/src/utils/ShareProduct.jsx
@@ -14,27 +14,33 @@ export default function ShareProduct() {
 
 	return (
 		<div className={styles.shareProduct}>
-			<div className='shareText'>Compartir</div>
+			<div className={styles.shareText}>Compartir</div>
 			<WhatsappShareButton
 				url={shareUrl}
 				title={'this is a title'}
 				separator={'this is a separator'}
+				iconFillColor={'transparent'}
 			>
-				<WhatsappIcon size={40} round={true} />
+				<WhatsappIcon className={styles.shareButton} size={40} round={true} />
 			</WhatsappShareButton>
 			<FacebookShareButton
 				url={shareUrl}
 				quote={'this is a qupte'}
 				hashtag={'this is a hashtag'}
 			>
-				<FacebookIcon iconFillColor={'white'} size={40} round={true} />
+				<FacebookIcon
+					className={styles.shareButton}
+					iconFillColor={'white'}
+					size={40}
+					round={true}
+				/>
 			</FacebookShareButton>
 			<LinkedinShareButton
 				url={shareUrl}
 				title={'this is a title'}
 				source={'this is a source'}
 			>
-				<LinkedinIcon size={40} round={true} />
+				<LinkedinIcon className={styles.shareButton} size={40} round={true} />
 			</LinkedinShareButton>
 		</div>
 	);
