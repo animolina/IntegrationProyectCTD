448b8bd861f679f805b74c5a9f638adccf7b15ad
Emilia Sartirana <emsartirana@gmail.com>
2022-11-08 22:54:00 -0300

    Ahora siii, perdon de nuevo

--- backend/src/main/java/com/example/piBack/PiBackApplication.java
+++ backend/src/main/java/com/example/piBack/PiBackApplication.java
@@ -20,7 +20,7 @@ public class PiBackApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry){
-				registry.addMapping("/*").allowedOrigins("*");
+				registry.addMapping("/**").allowedOrigins("*");
 
 			}
 
