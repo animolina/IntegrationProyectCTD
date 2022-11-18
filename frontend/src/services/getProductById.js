907bd0133950a27952e0fbf341810aa0b8384365
nkwaaaa <nkwaaaa@gmail.com>
2022-11-18 20:23:12 -0300

    update getProduct url

--- frontend/src/services/getProductById.js
+++ frontend/src/services/getProductById.js
@@ -3,7 +3,10 @@ import { ajax } from '../utils/ajax';
 export const getProductById = async id => {
 	const optionsRequest = {
 		method: 'GET',
-		url: `http://ec2-3-91-229-168.compute-1.amazonaws.com:8080/${id}`,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+		},
+		url: `http://ec2-3-91-229-168.compute-1.amazonaws.com:8080/product/${id}`,
 	};
 	return await ajax(optionsRequest);
 };
