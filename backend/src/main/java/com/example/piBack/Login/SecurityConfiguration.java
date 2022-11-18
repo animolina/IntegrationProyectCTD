d824d7b2ff1268e916833646d65c82488f1907f4
animolina <anapaulamolinalopez@gmail.com>
2022-11-18 19:40:41 -0300

    update imports in security configuration

--- backend/src/main/java/com/example/piBack/Login/SecurityConfiguration.java
+++ backend/src/main/java/com/example/piBack/Login/SecurityConfiguration.java
@@ -3,7 +3,6 @@ import com.example.piBack.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
