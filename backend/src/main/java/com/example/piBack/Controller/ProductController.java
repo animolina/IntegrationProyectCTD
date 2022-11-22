180bae90bcb4b49072d84e123970eec53405b871
virginia <vgiardino@outlook.es>
2022-11-21 21:23:01 -0300

    arreglo import

--- backend/src/main/java/com/example/piBack/Controller/ProductController.java
+++ backend/src/main/java/com/example/piBack/Controller/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
