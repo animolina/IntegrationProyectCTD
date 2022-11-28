a245788381f11312f5af25da7043afe49b8ca856
animolina <anapaulamolinalopez@gmail.com>
2022-11-28 12:08:43 -0300

    BACKEND: add query to get reservation by product id

--- backend/src/main/java/com/example/piBack/Controller/ReservationController.java
+++ backend/src/main/java/com/example/piBack/Controller/ReservationController.java
@@ -1,5 +1,4 @@
 package com.example.piBack.Controller;
-
 import com.example.piBack.Model.Client;
 import com.example.piBack.Model.Reservation;
 import com.example.piBack.Model.User;
@@ -9,10 +8,7 @@ import com.example.piBack.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Collection;
 import java.util.Optional;
 
@@ -23,13 +19,15 @@ public class ReservationController {
     private ReservationService reservationService;
     private UserService userService;
     private ClientService clientService;
+
     @Autowired
     public ReservationController(ReservationService reservationService, UserService userService, ClientService clientService) {
         this.reservationService = reservationService;
         this.userService = userService;
         this.clientService = clientService;
-    }
 
+    }
+    //add reservation
     @PostMapping
     public ResponseEntity<Object> addReservation(@RequestBody Reservation reservation) {
         User user = userService.getUserFromSecurityContextHolder();
@@ -37,26 +35,35 @@ public class ReservationController {
         reservation.setClient(client);
         try {
             Reservation newReservation = reservationService.addReservation(reservation);
-            return new ResponseEntity<>("Reservation ID: "+newReservation.getId()+" created", HttpStatus.OK);
-        }catch (Exception e) {
+            return new ResponseEntity<>("Reservation ID: " + newReservation.getId() + " created", HttpStatus.OK);
+        } catch (Exception e) {
             return new ResponseEntity<>("Server error", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
+    //get all reservations
     @GetMapping
     public ResponseEntity<Collection<Reservation>> listReservation() {
         return ResponseEntity.ok(reservationService.listReservation());
     }
-
+    //get reservation by id
     @GetMapping("/{id}")
-    public ResponseEntity<Reservation> findReservation(@PathVariable Long id){
-        if(reservationService.findReservation(id).isPresent()) {
+    public ResponseEntity<Reservation> findReservation(@PathVariable Long id) {
+        if (reservationService.findReservation(id).isPresent()) {
             return ResponseEntity.ok(reservationService.findReservation(id).get());
-        }else{
-            return new ResponseEntity("Reservation with id "+id+" not found", HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity("Reservation with id " + id + " not found", HttpStatus.NOT_FOUND);
         }
     }
-
+    //get reservations by product id
+    @GetMapping("/productId/{productId}")
+    public ResponseEntity<Collection<Reservation>>  findReservationsByProductId(@PathVariable Long productId) {
+        if(reservationService.findReservationsByProductId(productId) != null){
+            return ResponseEntity.ok(reservationService.findReservationsByProductId(productId));
+        }else {
+            return new ResponseEntity("The product you are trying to get reservations from does not exist", HttpStatus.NOT_FOUND);
+        }
+}
+    //update reservation by id
     @PutMapping("/{id}")
     public ResponseEntity<Reservation> editReservation(@PathVariable("id") long id, @RequestBody Reservation reservation) {
         Optional<Reservation> reservation_ = reservationService.findReservation(id);
@@ -67,7 +74,7 @@ public class ReservationController {
             return new ResponseEntity("Reservation with id "+id+" not found", HttpStatus.NOT_FOUND);
         }
     }
-
+    //delete reservation by id
     @DeleteMapping("/{id}")
     public ResponseEntity deleteReservation (@PathVariable Long id) {
         try {
--- backend/src/main/java/com/example/piBack/Repository/ReservationRepository.java
+++ backend/src/main/java/com/example/piBack/Repository/ReservationRepository.java
@@ -1,7 +1,10 @@
 package com.example.piBack.Repository;
-
 import com.example.piBack.Model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.Collection;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    @Query("SELECT r FROM Reservation r WHERE r.product.id= ?1")
+    Collection<Reservation> findReservationsByProductId(Long productId);
 }
